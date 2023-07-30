0cef4934d4e0471de20a8a3200619ee3073f203d
Rene Pohland <rene@webograph.de>
2023-07-30 21:35:27 +0200

    import map and template moved to own file

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-import express from "https://esm.sh/express@4.18.2";
-import cors from "https://esm.sh/cors@2.8.5";
-import compression from "https://esm.sh/compression@1.7.4";
-import {
-  Request,
-  Response,
-} from "https://esm.sh/express-serve-static-core@0.1.1";
+import express from "express";
+import cors from "cors";
+import compression from "compression";
+import { Request, Response } from "express-serve-static-core";
 import { createNeutralPalette, createTonalPalette } from "./palette.ts";
+import { template } from "./template.ts";
 
 const port = Deno.env.get("PORT") || "8080";
 const app = express();
@@ -32,33 +30,6 @@ app.get(["/neutral", "/neutral/:color"], (req: Request, res: Response) => {
   const palette = createNeutralPalette(color ? "#" + color : undefined);
   res.json(palette);
 });
-
-const endpoints = `
-<h2>Available endpoints</h2>
-<p>[color] is a hexadecimal CSS color without the #</p>
-<ul>
-  <li>/tonal/[color]</li>
-  <li>/neutral</li>
-  <li>/neutral/[color]</li>
-</ul>
-`;
-
-const template = (title: string) => `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="color-scheme" content="light dark">
-    <meta charset="UTF-8">
-    <title>${title}</title>
-  </head>
-</html>
-<body>
-  <h1>${title}</h1>
-  ${endpoints}
-</body>
-</html>
-`;
 
 app.use("*", (req: Request, res: Response) => {
   const isHome = req.originalUrl === "/";
