b913ba850186c02163478fe01502f9bcc95b3e74
Rene Pohland <rene@webograph.de>
2023-08-06 05:10:11 +0200

    custom error export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,39 +1,36 @@
-export function renameProperties<T extends object>(
-  name: string,
-  replace: string,
-  obj: T
-): T {
-  const renamed: { [key: string]: unknown } = {};
+export * from './custom-error.ts'
+
+export function renameProperties<T extends object>(name: string, replace: string, obj: T): T {
+  const renamed: { [key: string]: unknown } = {}
 
   for (const key of Object.keys(obj)) {
     if (key.includes(name)) {
-      renamed[key.replace(name, replace)] = obj[key as keyof T];
-      continue;
+      renamed[key.replace(name, replace)] = obj[key as keyof T]
+      continue
     }
 
     const capitalizedName = name
-      .split("")
+      .split('')
       .map((c, i) => (i === 0 ? c.toUpperCase() : c))
-      .join("");
+      .join('')
 
     if (key.includes(capitalizedName)) {
       const capitalizedReplace = replace
-        .split("")
+        .split('')
         .map((c, i) => (i === 0 ? c.toUpperCase() : c))
-        .join("");
-      renamed[key.replace(capitalizedName, capitalizedReplace)] =
-        obj[key as keyof T];
-      continue;
+        .join('')
+      renamed[key.replace(capitalizedName, capitalizedReplace)] = obj[key as keyof T]
+      continue
     }
 
-    renamed[key] = obj[key as keyof T];
+    renamed[key] = obj[key as keyof T]
   }
 
-  return renamed as T;
+  return renamed as T
 }
 
 export function capitilize(str: string): string {
   return str.split('')
     .map((c, i, a) => (i === 0 || a[i - 1] === ' ' ? c.toUpperCase() : c))
-    .join('');
+    .join('')
 }
