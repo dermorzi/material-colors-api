51ef04cae897ba5781518e606ec961b52c5d2e8d
Rene Pohland <rene@webograph.de>
2023-08-06 05:06:11 +0200

    better constants

--- src/material/constants.ts
+++ src/material/constants.ts
@@ -1,49 +1,45 @@
-export const TONES = [
-  0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 100,
-] as const;
+import { RolesMap } from './types.ts'
 
-export const TONE_ROLES = {
-  light: {
-    primary: 40,
-    onPrimary: 100,
-    primaryContainer: 90,
-    onPrimaryContainer: 10,
-  },
-  dark: {
-    primary: 80,
-    onPrimary: 20,
-    primaryContainer: 30,
-    onPrimaryContainer: 90,
-  },
-} as const;
+function extractTonesFromMap(map: RolesMap) {
+  const tones = new Set()
 
-export const NEUTRAL_TONE_ROLES = {
-  light: {
-    surfaceDim: 87,
-    surface: 98,
-    surfaceBright: 98,
-    surfaceContainerLowest: 100,
-    surfaceContainerLow: 96,
-    surfaceContainer: 94,
-    surfaceContainerHigh: 92,
-    surfaceContainerHighest: 90,
-    onSurface: 10,
-    onSurfaceVariant: 30,
-    onOutline: 50,
-    onOutlineVariant: 80,
-  },
-  dark: {
-    surfaceDim: 6,
-    surface: 6,
-    surfaceBright: 24,
-    surfaceContainerLowest: 4,
-    surfaceContainerLow: 10,
-    surfaceContainer: 12,
-    surfaceContainerHigh: 17,
-    surfaceContainerHighest: 22,
-    onSurface: 90,
-    onSurfaceVariant: 80,
-    onOutline: 60,
-    onOutlineVariant: 30,
-  },
-} as const;
+  for (const [_, light, dark] of Object.values(map)) {
+    if (typeof light !== 'number' || typeof dark !== 'number') {
+      continue
+    }
+
+    tones.add(light).add(dark)
+  }
+
+  return Array.from(tones).sort() as number[]
+}
+
+export const SUPPORTED_COLOR_FORMATS = ['hex', 'rgb', 'rgba', 'hsl'] as const
+
+export const PALETTE_NAMES = ['primary', 'secondary', 'tertiary', 'neutral', 'neutralVariant', 'error'] as const
+
+export const NEUTRAL_ROLES: Readonly<RolesMap> = {
+  surfaceDim: ['neutral', 87, 6],
+  surface: ['neutral', 98, 6],
+  surfaceBright: ['neutral', 98, 24],
+  surfaceContainerLowest: ['neutral', 100, 4],
+  surfaceContainerLow: ['neutral', 96, 10],
+  surfaceContainer: ['neutral', 94, 12],
+  surfaceContainerHigh: ['neutral', 92, 17],
+  surfaceContainerHighest: ['neutral', 90, 22],
+  onSurface: ['neutralVariant', 10, 90],
+  onSurfaceVariant: ['neutralVariant', 30, 80],
+  outline: ['neutralVariant', 50, 60],
+  outlineVariant: ['neutralVariant', 80, 30],
+}
+
+export const NEUTRAL_TONES = [...extractTonesFromMap(NEUTRAL_ROLES)] as const
+
+export const TONAL_ROLES: Readonly<RolesMap> = {
+  name: ['primary', 40, 80],
+  onName: ['primary', 100, 20],
+  nameContainer: ['primary', 90, 30],
+  onNameContainer: ['primary', 10, 90],
+}
+
+export const TONAL_TONES = [...extractTonesFromMap(TONAL_ROLES)] as const
