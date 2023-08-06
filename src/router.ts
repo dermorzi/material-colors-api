081c99e0d0168eb77ee7ddc97235e14b9eb9bc33
Rene Pohland <rene@webograph.de>
2023-08-06 12:49:28 +0200

    routes for multiple output formats

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,20 @@
-import { Route } from 'libs'
+import { ParsedRequest, Route } from 'libs'
 import { createTheme } from 'material'
-import CSSGenerator from './generators/css.ts'
+import { CSSGenerator, LessGenerator, SASSGenerator, SCSSGenerator, StylusGenerator } from 'generators'
 
 export const root = new Route('/')
 
-root.add('/theme').get(({ query }) => {
+function requestTheme(query: ParsedRequest['query']) {
+  const theme = createTheme(query)
+
+  const target = query.target && typeof query.target === 'string'
+    ? query.target
+    : Array.isArray(query.target) ? query.target[0] : 'hex'
+
+  return { theme, target }
+}
+
+root.add('/theme.json').get(({ query }) => {
   const theme = createTheme(query)
 
   if (theme instanceof Error) {
@@ -19,18 +29,68 @@ root.add('/theme').get(({ query }) => {
 })
 
 root.add('/theme.css').get(({ query }) => {
-  const theme = createTheme(query)
+  const { theme, target } = requestTheme(query)
 
   if (theme instanceof Error) {
     const { message } = theme as Error
     return new Response(message, { status: 400 })
   }
 
-  const target = query.target && typeof query.target === 'string'
-    ? query.target
-    : Array.isArray(query.target) ? query.target[0] : 'hex'
+  const gen = new CSSGenerator(theme, target, query?.prefix)
 
-  return new Response(CSSGenerator(theme, target))
+  return new Response(gen.build())
+})
+
+root.add('/theme.scss').get(({ query }) => {
+  const { theme, target } = requestTheme(query)
+
+  if (theme instanceof Error) {
+    const { message } = theme as Error
+    return new Response(message, { status: 400 })
+  }
+
+  const gen = new SCSSGenerator(theme, target, query?.prefix)
+
+  return new Response(gen.build())
+})
+
+root.add('/theme.sass').get(({ query }) => {
+  const { theme, target } = requestTheme(query)
+
+  if (theme instanceof Error) {
+    const { message } = theme as Error
+    return new Response(message, { status: 400 })
+  }
+
+  const gen = new SASSGenerator(theme, target, query?.prefix)
+
+  return new Response(gen.build())
+})
+
+root.add('/theme.less').get(({ query }) => {
+  const { theme, target } = requestTheme(query)
+
+  if (theme instanceof Error) {
+    const { message } = theme as Error
+    return new Response(message, { status: 400 })
+  }
+
+  const gen = new LessGenerator(theme, target, query?.prefix)
+
+  return new Response(gen.build())
+})
+
+root.add('/theme.stylus').get(({ query }) => {
+  const { theme, target } = requestTheme(query)
+
+  if (theme instanceof Error) {
+    const { message } = theme as Error
+    return new Response(message, { status: 400 })
+  }
+
+  const gen = new StylusGenerator(theme, target, query?.prefix)
+
+  return new Response(gen.build())
 })
 
 root.get(async ({ path }) => {
